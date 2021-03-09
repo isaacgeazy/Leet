6e09412ba6ca4aafe147223df5594660f6904974
Isaac <isaacgeazy@gmail.com>
2021-03-09 11:23:56 -0300

    Atividade Leet

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diga;
+
+            Console.Write("DIGA ALGO: ");
+            diga = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"{diga.Replace("a","4").Replace("e","3").Replace("i","1").Replace("o","0").Replace("t","7").Replace("s","5")}");
+            
+        }
+    }
+}
