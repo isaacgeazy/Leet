0e30756678bc2f41fb38ccc84291f532b568762a
Diego Neri <diego@diegoneri.com>
2021-03-12 18:46:05 -0300

    Adiciona letra l e letras maiúsculas

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Leet
             Console.Write("DIGA ALGO: ");
             diga = Convert.ToString(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine($"{diga.Replace("a","4").Replace("e","3").Replace("i","1").Replace("o","0").Replace("t","7").Replace("s","5")}");
+            Console.WriteLine($"{diga.Replace("a","4").Replace("A","4").Replace("e","3").Replace("E","3").Replace("i","1").Replace("I","1").Replace("l","1").Replace("L","1").Replace("o","0").Replace("O","0").Replace("t","7").Replace("T","7").Replace("s","5").Replace("S","5")}");
             
         }
     }
